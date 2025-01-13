3884958254d3656d0d44c6b6c97c38faab1e0992
jeonhyelim <114721039+jeonhyelim@users.noreply.github.com>
2025-01-14 01:15:52 +0900

    [Bronze II] Title: 수 정렬하기, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -4,12 +4,7 @@ array = list()
 for _ in range(n):
     array.append(int(input()))
     
-for i in range(n):
-    min_index = i
-    for j in range(i+1, n):
-        if array[min_index] > array[j]:
-            min_index = j
-    array[i], array[min_index] = array[min_index], array[i]
+array.sort() #기본 정렬 라이브러리 - 오름차순
 
 for i in array:
     print(i)
