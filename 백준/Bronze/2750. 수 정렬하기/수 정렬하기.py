abd782358486248487f36128051f0c4f3861dda4
jeonhyelim <114721039+jeonhyelim@users.noreply.github.com>
2025-01-14 01:17:23 +0900

    Update 수 정렬하기.py

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -3,8 +3,17 @@ array = list()
 
 for _ in range(n):
     array.append(int(input()))
-    
+
+#1
 array.sort() #기본 정렬 라이브러리 - 오름차순
+
+#2
+for i in range(n):
+    min_index = i
+    for j in range(i+1, n):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i]
 
 for i in array:
     print(i)
