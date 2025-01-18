7f6bd3af92620475092ffd90a7746828fc8c498d
jeonhyelim <114721039+jeonhyelim@users.noreply.github.com>
2025-01-18 18:17:16 +0900

    Update 좌표 정렬하기.py

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -2,8 +2,13 @@ n = int(input())
 arr = []
 
 for _ in range(n):
+    #1
     input_data = input().split(' ')
     arr.append( (int(input_data[0]),int(input_data[1])) )
+
+    #2
+    x, y = map(int, input().split(' '))
+    arr.append((x,y))
 
 arr = sorted(arr)
 #key x, 기본 정렬 라이브러리
