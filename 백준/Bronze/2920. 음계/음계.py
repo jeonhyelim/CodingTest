542c336a544043d4efe0c4beed7991416ddced00
jeonhyelim <114721039+jeonhyelim@users.noreply.github.com>
2025-01-23 17:14:04 +0900

    Update 음계.py

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -3,7 +3,7 @@ a = list(map(int,input().split(' ')))
 ascending = True
 descending = True
 
-for i in range(1,8): 
+for i in range(1,8): #1~7
     if a[i] > a[i-1]: #a1>a0 오름차순
         descending = False
     elif a[i] < a[i-1]: #a1<a0 내림차순
