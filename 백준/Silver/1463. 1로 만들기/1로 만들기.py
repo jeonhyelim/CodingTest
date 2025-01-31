b7f8077d42f30d70127fe5758f6f5cd817b3f85f
jeonhyelim <114721039+jeonhyelim@users.noreply.github.com>
2025-01-31 20:29:45 +0900

    Update 1로 만들기.py

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -11,7 +11,50 @@ for i in range(2, x+1): #2~x
 
 print(d[x])
 
+
 #2.DP - Topdown 풀이 (재귀)
+x = int(input())
+dp = {1:0} #딕셔너리로 초기화. 1일때 연산횟수 0 가짐
+
+def rec(n): #재귀적으로 dp 딕셔너리 채워나감
+    if n in dp.keys(): #종료조건; 입력받은 n이 dp딕셔너리의 key에 존재하는 경우
+        return dp[n]
+        
+    if (n%3 == 0) and (n%2 == 0):
+        dp[n] = min(rec(n//3)+1, rec(n//2)+1)
+    elif n%3 == 0:
+        dp[n] = min(rec(n//3)+1, rec(n-1)+1)
+    elif n%2 == 0:
+        dp[n] = min(rec(n//2)+1, rec(n-1)+1)
+    else:
+        dp[n] = rec(n-1) + 1
+    return dp[n]
+    
+print(rec(x))
 
 
-#3.BFS
+#3.BFS - 최단거리
+from collections import deque
+
+x = int(input())
+Q = deque([x]) #큐 초기화 ; 시작점:x
+visited = [0]*(x+1) #연산 횟수 저장(방문한 배열)
+
+while Q:
+    c = Q.popleft() #현재 숫자
+    
+    if c == 1:
+        break
+        
+    if c%3 == 0 and visited[c//3] == 0:
+        Q.append(c//3)
+        visited[c//3] = visited[c]+1
+    if c%2 == 0 and visited[c//2] == 0:
+        Q.append(c//2)
+        visited[c//2] = visited[c]+1
+    if visited[c-1] == 0:
+        Q.append(c-1)
+        visited[c-1] = visited[c]+1
+        
+print(visited[1])
+
