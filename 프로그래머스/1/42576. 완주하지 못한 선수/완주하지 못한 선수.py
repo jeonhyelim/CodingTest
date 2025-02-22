3e69fb892d1d9f4215f521a4a2bc0a407fad271f
jeonhyelim <114721039+jeonhyelim@users.noreply.github.com>
2025-02-22 20:12:16 +0900

    Update 완주하지 못한 선수.py

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -6,3 +6,17 @@ def solution(participant, completion):
             return i
         
     return participant[-1]
+
+#2
+def solution(participant, completion):
+    answer = ''
+    hash_dict={} // hash dictionary
+    sumHash=0
+    for i in participant:
+        hash_dict[hash(i)]=i // participant hash인덱스 넣기
+        sumHash+=hash(i) // hash 총합
+        
+    for j in completion:
+        sumHash-=hash(j) // completion의 hash값들 빼기
+        
+    return hash_dict[sumHash] // 남은 hash값에 해당하는 선수가 완주 못한 선수
