2ec9f4fafcf9b228eb8a63263ac7abcea1347819
jeonhyelim <114721039+jeonhyelim@users.noreply.github.com>
2025-04-28 22:02:45 +0900

    Update 트리.py

--- 백준/Gold/1068. 트리/트리.py
+++ 백준/Gold/1068. 트리/트리.py
@@ -12,7 +12,7 @@ for idx in range(n):
         continue
     child[parent[idx]].append(idx) # 나머지 노드는 부모의 자식으로 추가 => child[x] x의 자식노드 리스트
     
-def dfs(d): # 삭제처리 delNode를 루트로 하는 서브트리 전부를 방문. d: 현재 방문한 노드 번호
+def dfs(d): # 삭제처리 . d: 현재 방문한 노드 번호
     # 리프 노드인 경우(자식x)
     if child[d] == []:
         child[d].append(-1) # 이 서브트리의 모든 노드의 child를 다 [-1]로 바꿔버림 = 삭제
@@ -21,7 +21,7 @@ def dfs(d): # 삭제처리 delNode를 루트로 하는 서브트리 전부를 �
     for c in child[d]:
         dfs(c) # 재귀
 
-dfs(delNode)
+dfs(delNode) #delNode를 루트로 하는 서브트리 전부를 방문
 
 # 리프 노드 개수 세기
 ans = 0
